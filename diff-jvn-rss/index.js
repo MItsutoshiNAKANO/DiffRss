69a432099ed0a094ddbc4186f749f3e3ff914a53
Mitsutoshi Nakano <ItSANgo@gmail.com>
2022-08-11 10:24:38 +0900

    installed durable-functions

--- diff-jvn-rss/index.js
+++ diff-jvn-rss/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const df = require("durable-functions");
 let RssPicker = require('../lib/parse-feeds');
 let rssPicker = new RssPicker();
 const URL = 'https://jvn.jp/rss/jvn.rdf';
+
+let previous = {};
 
 module.exports = async function (context, myTimer) {
     const feed = await rssPicker.body.parseURL(URL);
