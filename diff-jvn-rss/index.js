a2ea46b2a04a9b1aa4c78d3dbce813e9033ff098
Mitsutoshi Nakano <ItSANgo@gmail.com>
2022-08-11 09:53:37 +0900

    separate common logic

--- diff-jvn-rss/index.js
+++ diff-jvn-rss/index.js
@@ -1,41 +1,11 @@
 'use strict';
 
-// https://github.com/rbren/rss-parser
-let Parser = require('rss-parser');
+let RssPicker = require('../lib/parse-feeds');
+let rssPicker = new RssPicker();
 const URL = 'https://jvn.jp/rss/jvn.rdf';
-let parser = new Parser({
-    customFields: {
-      feed: [['dcterms:modified', 'modified']],
-      item: [['dcterms:modified', 'modified']],
-    }
-});
-
-function pickupJvnRss(feed) {
-    let result = new Map();
-    result['modified'] = feed.modified;
-    let items = new Array();
-    feed.items.forEach(function (entry) {
-        items.unshift({
-            'modified': entry.modified,
-            'link': entry.link,
-            'title': entry.title
-        });
-    });
-    result['items'] = items;
-    return result;
-}
-
-// // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/async_function
-// (async () => {
-//     const current = pickupJvnRss(await parser.parseURL(URL));
-//     console.log(current);
-// })();
 
 module.exports = async function (context, myTimer) {
-    if (myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    const current = pickupJvnRss(await parser.parseURL(URL));
-    console.log(current);
+    const feed = await rssPicker.body.parseURL(URL);
+    const current = RssPicker.pickup(feed);
+    context.log(current);
 };
