f6e4ef49f018af297eac6d83bd4cdec9dcfd06a5
Mitsutoshi Nakano <ItSANgo@gmail.com>
2022-08-12 09:19:49 +0900

    get modified

--- diff-jvn-rss/index.js
+++ diff-jvn-rss/index.js
@@ -1,14 +1,8 @@
 'use strict';
 
-const df = require("durable-functions");
-let RssPicker = require('../lib/parse-feeds');
-let rssPicker = new RssPicker();
-const URL = 'https://jvn.jp/rss/jvn.rdf';
+const Rss = require('../lib/parse-feeds');
 
-let previous = {};
-
-module.exports = async function (context, myTimer) {
-    const feed = await rssPicker.body.parseURL(URL);
-    const current = RssPicker.pickup(feed);
-    context.log(current);
+module.exports = async function (context, timer, saver) {
+    const SITE =  { 'url': 'https://jvn.jp/rss/jvn.rdf', 'key': 'jvn' };
+    return await Rss.modified(context, SITE, { 'timer': timer, 'saver': saver });
 };
