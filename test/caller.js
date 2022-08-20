3751d58014fc39b27367fc4b69be98a7f6aec35e
Mitsutoshi Nakano <ItSANgo@gmail.com>
2022-08-21 07:52:54 +0900

    rename the folder

--- test/caller.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Watcher = require('../lib/rss-watcher.js')
-
-const dummy = new Watcher(console, {
-  url: 'https://example.com/', key: 'test'
-}, null)
-
-dummy.notice(null, null)
