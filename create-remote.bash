1d2e0a4f6995fd9dbadce62bc223535fdd84eb09
Mitsutoshi Nakano <ItSANgo@gmail.com>
2022-08-21 11:22:49 +0900

    add creating remote resources scripts

---
+++ create-remote.bash
@@ -0,0 +1,22 @@
+#! /bin/bash -eu
+
+echo '1st, you must login to Azure. Type: 
+    az login
+See https://docs.microsoft.com/ja-jp/azure/azure-functions/create-first-function-cli-node?tabs=azure-cli%2Cbrowser
+' >&2
+
+source './secrets/create-remote.rc.bash'
+
+app_name='DiffRss'
+function_ver='4'
+node_ver='16'
+os='Linux'
+
+az group create --name "$resource_group" --location "$location"
+az storage account create --name "$storage_name" --location "$location" --resource-group "$resource_group" --sku "$sku"
+az functionapp create --resource-group "$resource_group" --consumption-plan-location "$location" --os-type "$os" --runtime node --runtime-version "$node_ver" --functions-version "$function_ver" --name "$app_name" --storage-account "$storage_name"
+
+echo "If you want to delete, you type:
+
+    az group delete --name $resource_group
+" >&2
