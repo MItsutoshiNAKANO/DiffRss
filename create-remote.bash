c582a7ac87991222b6770f14459f1e8d4b3565e1
Mitsutoshi Nakano <ItSANgo@gmail.com>
2023-07-30 22:35:12 +0900

    be explicit the hidden file.

--- create-remote.bash
+++ create-remote.bash
@@ -1,16 +1,22 @@
 #! /bin/bash -eu
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 echo '1st, you must login to Azure. Type: 
     az login
 See https://docs.microsoft.com/ja-jp/azure/azure-functions/create-first-function-cli-node?tabs=azure-cli%2Cbrowser
 ' >&2
 
+# Define them in './secrets/create-remote.rc.bash'
+# resource_group='DiffRss-rg'
+# location='japanwest'
+# storage_name='diffrssstorage'
+# sku='Standard_LRS'
 source './secrets/create-remote.rc.bash'
 
 app_name='DiffRss'
 function_ver='4'
-node_ver='16'
-os='Linux'
+node_ver='18'
+os='Windows'
 
 az group create --name "$resource_group" --location "$location"
 az storage account create --name "$storage_name" --location "$location" --resource-group "$resource_group" --sku "$sku"
